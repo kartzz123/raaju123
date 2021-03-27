0557604d2e21990138fdb383e423a90f1adaf5eb
kartzz123 <kartzz282@gmail.com>
2021-03-27 18:57:00 +0530

    my commit the code

---
+++ andoperator.sh
@@ -0,0 +1,16 @@
+#! bin/bash 
+
+age=25
+
+if [ "$age" -gt 20  -a   "$age" -lt 30 ]
+
+then
+
+	echo "it is virtusa"
+
+else
+
+	echo "it is not"
+
+fi
+
---
+++ myfile.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+age=50
+
+if [ "$age" -gt 50 ] && [ "$age" -lt 60 ]
+then
+	echo "it is virtusa"
+else 
+	echo "it is not"
+fi
+
